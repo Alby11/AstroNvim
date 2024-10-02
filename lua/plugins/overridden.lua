1bf69b67bcc5916d967f6386781fd63dcbc673ee
alby11 <alby11@duck.com>
2024-10-02 12:36:30 +0200

    add better-escape override: kj

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -1,1 +1,31 @@
-return {}
+return {
+  {
+    "max397574/better-escape.nvim",
+    opts = function(plugin, opts)
+      -- check if an `indent` table exists, if not, create it
+      if not opts.mappings then opts.mappings = {} end
+      if not opts.mappings.i then opts.mappings.i = {} end
+      if not opts.mappings.i then opts.mappings.c = {} end
+      if not opts.mappings.i then opts.mappings.t = {} end
+      if not opts.mappings.i then opts.mappings.v = {} end
+      if not opts.mappings.i then opts.mappings.s = {} end
+      -- once we know it is created, we can set the sub-keys
+      opts.mappings.i.k = {
+        -- These can all also be functions
+        j = "<Esc>",
+      }
+      opts.mappings.c.k = {
+        j = "<Esc>",
+      }
+      opts.mappings.t.k = {
+        j = "<C-\\><C-n",
+      }
+      opts.mappings.v.k = {
+        j = "<Esc>",
+      }
+      opts.mappings.s.k = {
+        j = "<Esc>",
+      }
+    end,
+  },
+}
