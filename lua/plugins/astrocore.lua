8b0e9f8bd84ef44b6d4c4d16f1bf03e93d179855
alby11 <alby11@duck.com>
2024-12-18 08:06:38 +0100

    add vim.opt.fillchars = { vert = "|", horiz = "-" }

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -38,6 +38,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        fillchars = { vert = "|", horiz = "-" }, -- Simple vertical line
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
