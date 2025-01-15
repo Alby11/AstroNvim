21f6f6450e34118d57bbd84baa47ae58150b917e
alby11 <alby11@duck.com>
2025-01-15 16:23:13 +0100

    add vertical/horizontal split separators

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -10,6 +10,49 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
+    autocmds = {
+      -- autocommands are organized into augroups for easy management
+      autohidetabline = {
+        -- each augroup contains a list of auto commands
+        {
+          -- create a new autocmd on the "User" event
+          event = "User",
+          -- the pattern is the name of our User autocommand events
+          pattern = "AstroBufsUpdated", -- triggered when vim.t.bufs is updated
+          -- nice description
+          desc = "Hide tabline when only one buffer and one tab",
+          -- add the autocmd to the newly created augroup
+          group = "autohidetabline",
+          callback = function()
+            -- if there is more than one buffer in the tab, show the tabline
+            -- if there are 0 or 1 buffers in the tab, only show the tabline if there is more than one vim tab
+            local new_showtabline = #vim.t.bufs > 1 and 2 or 1
+            -- check if the new value is the same as the current value
+            if new_showtabline ~= vim.opt.showtabline:get() then
+              -- if it is different, then set the new `showtabline` value
+              vim.opt.showtabline = new_showtabline
+            end
+          end,
+        },
+      },
+      PrivateJrnl = {
+        {
+          event = "User",
+          pattern = "BufNewFile,BufReadPre,*.jrnl",
+          callback = function()
+            vim.o.shada = ""
+            vim.o.swapfile = false
+            vim.o.undofile = false
+            vim.o.backup = false
+            vim.o.writebackup = false
+            vim.o.shelltemp = false
+            vim.o.history = 0
+            vim.o.modeline = false
+            vim.o.secure = true
+          end,
+        },
+      },
+    },
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
