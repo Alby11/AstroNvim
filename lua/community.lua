05f7f294c5bcdfee28fdbfdb67d25945e229b750
Alberto Tallone <alberto.tallone@racing-unleashed.com>
2025-06-30 15:42:11 +0200

    disable rest.nvim for win

--- lua/community.lua
+++ lua/community.lua
@@ -67,7 +67,7 @@ return {
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.programming-language-support.csv-vim" },
   { import = "astrocommunity.programming-language-support.nvim-jqx" },
-  { import = "astrocommunity.programming-language-support.rest-nvim" },
+  -- { import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.recipes.neovide" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   { import = "astrocommunity.recipes.vscode" },
