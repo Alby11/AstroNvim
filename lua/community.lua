3137fe06ffb6eec24d4feccf3e671247ac4c5854
Alberto Tallone <Alby11@users.noreply.github.com>
2025-07-29 08:52:46 +0200

    refactor(community): comment out ansible import in community modules

--- lua/community.lua
+++ lua/community.lua
@@ -53,7 +53,7 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.note-taking.neorg" },
-  { import = "astrocommunity.pack.ansible" },
+  -- { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
