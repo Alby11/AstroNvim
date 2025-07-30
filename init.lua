06dd54d754de8fd128fca4a12f750b5ad98db2fd
Alberto Tallone <Alby11@users.noreply.github.com>
2025-07-30 14:46:33 +0200

    feat(community): implement conditional rocks.nvim installer and enable rest.nvim support

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Conditional rocks.nvim installation
+require("user.rocks-installer").init()
+
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
