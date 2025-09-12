7fe42f66a43adcb2222039f2a11bcb14db8cf3af
Alby11 <17138674+Alby11@users.noreply.github.com>
2025-09-12 18:37:26 +0200

    fix(overridden): disable hererocks in plugin options for consistency

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -3,10 +3,14 @@ return {
     "folke/lazy.nvim",
     opts = function(opts)
       opts.rocks = {
-        hererocks = true,
+        hererocks = false,
         enabled = false,
       }
-    end
+    end,
+  },
+  {
+    "toppair/peek.nvim",
+    opts = function(_, opts) opts.app = "browser" end, -- "browser" or "webview"
   },
   {
     "max397574/better-escape.nvim",
@@ -36,5 +40,5 @@ return {
   {
     "kevinhwang91/nvim-ufo",
     enabled = false,
-  }
+  },
 }
