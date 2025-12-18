3ef4373b36a4c495d34f90d697110fa504984ca3
Alberto Tallone <17138674+Alby11@users.noreply.github.com>
2025-12-18 12:35:14 +0100

    feat(neovide): add initial configuration for Neovide plugin with options for font and scaling

---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,30 @@
+if not vim.g.neovide then
+  return {} -- do nothing if not in a Neovide session
+end
+
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    options = {
+      opt = { -- configure vim.opt options
+        -- configure font
+        guifont = "Cascadia Code:h12",
+        -- line spacing
+        linespace = 0,
+      },
+      g = { -- configure vim.g variables
+        -- configure scaling
+        neovide_scale_factor = 1.0,
+        -- configure padding
+        neovide_padding_top = 0,
+        neovide_padding_bottom = 0,
+        neovide_padding_right = 0,
+        neovide_padding_left = 0,
+        neovide_opacity = 0.7,
+        neovide_normal_opacity = 0.7,
+        neovide_position_animation_length = 0.15
+      },
+    },
+  },
+}
